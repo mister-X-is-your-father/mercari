aa076ef9c978b489fe5b73d1f8e38139c8d2a3eb
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-09-08 17:57:44 +0900

    マイグレーションファイル記入①

--- db/migrate/20190908081603_create_user_addresses.rb
+++ db/migrate/20190908081603_create_user_addresses.rb
@@ -1,7 +1,12 @@
 class CreateUserAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :user_addresses do |t|
-
+      t.references  :user_id      null: false, foreign_key: true
+      t.string      :postal_code
+      t.string      :region
+      t.string      :city
+      t.string      :block
+      t.string      :buildings
       t.timestamps
     end
   end
--- db/migrate/20190908081625_create_deliveries.rb
+++ db/migrate/20190908081625_create_deliveries.rb
@@ -1,7 +1,11 @@
 class CreateDeliveries < ActiveRecord::Migration[5.2]
   def change
     create_table :deliveries do |t|
-
+      t.integer     :payee      null: false
+      t.references  :region_id  null: false, foreign_key: true
+      t.integer     :time       null: false
+      t.references  :item_id    null: false, foreign_key: true
+      t.integer     :method     null: false
       t.timestamps
     end
   end
