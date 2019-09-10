8aabb124c81fcc69a5ffe8c4ebed0011c83bf113
HiroshiMorita <ikimonogakaru@gmail.com>
2019-09-10 13:55:17 +0900

    自動デプロイ導入

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,28 @@ set :repo_url, "git@example.com:HiroshiMorita/mercari.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],#😨ここは個人の公開鍵を指定してください
+                  keys: ['~/.ssh/morita.pem']#😨ここは個人の秘密鍵を指定してください
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
