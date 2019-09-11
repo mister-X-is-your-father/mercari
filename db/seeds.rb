b62eb58642fd8fe3279405aea48f3dcc85a465be
Toya Kawakami <tojyapan925@gmail.com>
2019-09-11 15:57:29 +0900

    カテゴリーのseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+
+# ブランド
+CSV.foreach('db/brand.csv') do |record|
+  Brand.create(name: record[0], brand_group_id: record[1])
+end
+
+# サイズ
+CSV.foreach('db/size.csv') do |record|
+  Size.create(name: record[0])
+end
