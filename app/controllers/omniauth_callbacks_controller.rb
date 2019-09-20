dded9fcbdcea82646d32cb937377e62b0487fe18
Toya Kawakami <tojyapan925@gmail.com>
2019-09-20 12:22:19 +0900

    facebook認証の実装

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     callback_for(:facebook)
   end
@@ -15,7 +15,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to signup_registration_path
+      redirect_to user_registration_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => {
-    :delivery_addresses => 'users/delivery_addresses',
-    :omniauth_callbacks => 'users/omniauth_callbacks'
-  }
+  devise_for :users, :skip => [:omniauth_callbacks]
+
+  devise_scope :user do
+    match '/signup/auth/facebook' => 'omniauth_callbacks#passthru', via: [:get, :post]
+    match '/signup/auth/facebook/callback' => 'omniauth_callbacks#facebook', via: [:get, :post]
+    match '/signup/auth/google' => 'omniauth_callbacks#passthru', via: [:get, :post]
+    match '/signup/auth/google/callback' => 'omniauth_callbacks#google', via: [:get, :post]
+  end
 
   root 'items#index'
 
@@ -25,7 +29,9 @@ Rails.application.routes.draw do
   resources :brands, only: [:index, :show]
   resources :user_reviewes, only: [:index, :create]
 
+
   resources :signup, only: [:index, :create] do
+
     collection do
       get 'index'
       get 'registration'
@@ -36,6 +42,8 @@ Rails.application.routes.draw do
       post 'done'
     end
   end
+
+
   get 'mypage', to: 'mypage/mypage#index', as: :mypage_top
 
   namespace :mypage do
