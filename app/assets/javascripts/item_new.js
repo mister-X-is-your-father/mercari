780225516612851187e7513c4a6861f414f099f0
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-09-25 15:18:02 +0900

    アップロード時の画像プレビュー表示

--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -1,29 +1,22 @@
 $(function(){
 
-  // var buildHTML = function(message){
-  //   var image = message.image.url == null ? "" : `<img src="${message.image.url}">` 
-  //   var html = `<div class="message" data-message-id='${message.id}'>
-  //                 <div class="message__upper">
-  //                   <div class="message__upper__name">
-  //                     ${message.user_name}
-  //                   </div>
-  //                   <div class="message__upper__time">
-  //                     ${message.created_at}
-  //                   </div>
-  //                 </div>
-  //                 <div class="message__text">
-  //                   <p class="lower-message__content">
-  //                     ${message.body}
-  //                   </p>
-  //                   ${image}
-  //                 </div>
-  //               </div>`
-  //   return html;
-  // };
+  function buildImage(loadedImageUrl){
+    var html =
+    `<li class="iu-preview-box">
+      <figure class="iu-preview-box__image">
+        <img src=${loadedImageUrl}>
+      </figure>
+      <div class="iu-preview-box__text">
+        <a>編集</a>
+        <a>削除</a>
+      </div>
+    </li>`
+    return html
+  };
 
   let DropArea1 = '.iu-image__container__drop-area'
   let DropArea2 = '.drop-area-second'
-  let ImgArea = '.iu-image__container__image-area'
+  let PreviewArea = '.iu-image__container__preview-area__images ul'
   let images_array = []
 
   $(DropArea1).on({'dragenter dragover' :function(e){
@@ -34,10 +27,13 @@ $(function(){
       let images = e.originalEvent.dataTransfer.files;
       for (let i = 0; i < images.length; i++) { 
         images_array.push(images[i]);
+        let fileReader = new FileReader();
+        fileReader.onload = function(e) {
+        var loadedImageUrl = e.target.result;
+        $(buildImage(loadedImageUrl,)).appendTo(PreviewArea).trigger("create");
+        };
+        fileReader.readAsDataURL(images[i]);
       }
-
-      let html = buildHTML(images[0]); //一旦0で仮置き 画像のurlを取得したい
-      $(ImgArea).append(html);
 
       if (images_array.length >= 5) {
         $(DropArea2).css({
@@ -52,7 +48,7 @@ $(function(){
     e.preventDefault();
     let formData = new FormData($(this).get(0));
     images_array.forEach(function(image, i){
-     formData.append(`item[images_attributes][${i}][image]` ,image)
+     formData.append(`item[images_attributes][${i}][image]`, image)
     });
     $.ajax({
       url:         '/items',
