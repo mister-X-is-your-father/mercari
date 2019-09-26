ed015711ba56c99c69f7fc767fea518f83001c64
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-09-26 16:15:53 +0900

    カテゴリー選択分岐機能実装完了

--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -99,16 +99,18 @@ $(function(){
     });
   })
 
+///////////////////////////////////////////////////////////////////////////////////
+
   //カテゴリー選択機能
   function buildCategory(category){
-    let html = `<option value="${category.name}">${category.name}</option>`
+    let html = `<option value="${category.id}">${category.name}</option>`
     return html
   };
 
   function appendChildCategory(insertHTML){
     let ChildCategoryHTML = 
       `<div class="select-wrap iu-child">
-        <select id="parent-categories" name="item[category_id]">
+        <select id="child-categories" name="item[category_id]">
           <option value="">---</option>
           ${insertHTML}
       </div>`
@@ -118,7 +120,7 @@ $(function(){
   function appendGrandChildCategory(insertHTML){
     let GrandChildCategoryHTML = 
       `<div class="select-wrap iu-grandchild">
-        <select id="parent-categories" name="item[category_id]">
+        <select id="grandchild-categories" name="item[category_id]">
           <option value="">---</option>
           ${insertHTML}
       </div>`
@@ -128,16 +130,17 @@ $(function(){
   //親カテゴリー選択時
   $('#parent-categories').on('change', function(e){
     e.preventDefault();
-    let parent_name = $(this).val();
-    if (parent_name != ''){
+    let parent_id = $(this).val();
+    if (parent_id != ''){
       $.ajax({
         url:         '/items/get_child_categories',
         type:        'GET',
-        data:        { parent_category: parent_name },
+        data:        { parent_category: parent_id },
         dataType:   'json'
       })
       .done(function(child){
         $('.select-wrap.iu-child').remove();
+        $('.select-wrap.iu-grandchild').remove();
         let insertHTML = '';
         child.forEach(function(child_category){
           insertHTML += buildCategory(child_category);
@@ -150,18 +153,19 @@ $(function(){
     }
     else {
       $('.select-wrap.iu-child').remove();
+      $('.select-wrap.iu-grandchild').remove();
     }
   });
 
   //子カテゴリー選択時
   $('.select-wraps').on('change', '.select-wrap.iu-child', function(e){
     e.preventDefault();
-    let child_name = $(this).children().val();
-    if (child_name != ''){
+    let child_id = $(this).children().val();
+    if (child_id != ''){
       $.ajax({
         url:         '/items/get_grandchild_categories',
         type:        'GET',
-        data:        { child_category: child_name },
+        data:        { child_category: child_id },
         dataType:   'json'
       })
       .done(function(grandchild){
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,8 @@ class ItemsController < ApplicationController
     @images = @item.images.build
     @sizes = Size.all
     @brands = Brand.all
-    @parent_categories_array = Category.where(ancestry: nil).pluck(:name)
+    # @parent_categories_array = Category.where(ancestry: nil).pluck(:name)
+    @parent_categories = Category.where(ancestry: nil)
     @child_categories_array = Category.where.not(ancestry: nil).pluck(:name)
     @grandchild_categories = Category.where.not(ancestry: nil)
     @regions = Region.all
@@ -50,7 +51,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    binding.pry
     respond_to do |format|
       if @item.save
         format.html{ redirect_to root_path }
@@ -85,11 +85,11 @@ class ItemsController < ApplicationController
   end
 
   def get_child_categories
-    @child_categories = Category.find_by(name: params[:parent_category]).children
+    @child_categories = Category.find_by(id: params[:parent_category]).children
   end
 
   def get_grandchild_categories
-    @grandchild_categories = Category.find_by(name: params[:child_category]).children
+    @grandchild_categories = Category.find_by(id: params[:child_category]).children
   end
 
   private
