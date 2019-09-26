da8029bccc6376342db05af5e1e569568faa9ce3
Toya Kawakami <tojyapan925@gmail.com>
2019-09-26 12:12:37 +0900

    category view 修正

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -2,9 +2,7 @@ $(function() {
   $('a[href^="#"]').click(function() {
     let speed = 500;
     let href = $(this).attr("href");
-    console.log(href)
     let target = $(href == "#" || href == "" ? 'html' : href);
-    console.log(target)
     let position = target.offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
