2ffd8ceadd9aa0738bd9ca0eaa3e74965290dad6
Toya Kawakami <tojyapan925@gmail.com>
2019-09-26 11:52:03 +0900

    カテゴリ機能のアルゴリズム設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,14 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @items = Item.where(category_id: @category.id).page(params[:page]).per(120).order("created_at DESC").includes(:images)
+    if @category.is_root?
+      parent = Category.find_by(id: @category.id).subtree
+      @items = Item.where(category_id: parent).page(params[:page]).per(120).order("created_at DESC").includes(:images)
+    elsif @category.has_parent? && @category.has_children?
+      child = Category.find_by(id: @category.id).subtree
+      @items = Item.where(category_id: child).page(params[:page]).per(120).order("created_at DESC").includes(:images)
+    else
+      @items = Item.where(category_id: @category.id).page(params[:page]).per(120).order("created_at DESC").includes(:images)
+    end
   end
 end
