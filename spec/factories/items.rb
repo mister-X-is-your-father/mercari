53a45b07164cc4cf73b4166e4cf3682f16b2a720
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-07 11:18:16 +0900

    factory_bot items.rb作成

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :item do
+    name                  {"コート"}
+    price                 {"5000"}
+    product_condition     {"1"}
+    description           {"00000000"}
+  end
+
+end
