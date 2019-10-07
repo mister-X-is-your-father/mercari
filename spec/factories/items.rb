8c81350b5a00a2dc6995789dad475d658f9f9086
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-07 11:29:37 +0900

    factory_bot items.rbに:item情報を記載

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,10 +1,20 @@
 FactoryBot.define do
 
   factory :item do
-    name                  {"コート"}
+    name                  {"ロングコート"}
     price                 {"5000"}
-    product_condition     {"1"}
-    description           {"00000000"}
+    product_condition     {"1"} #新品、未使用
+    description           {"レディースのロングコートです"}
+    sold_condition        {"1"} #出品中
+    category_id           {"32"} #ロングコート
+    region_id             {"13"} #東京都
+    brand_id              {"5788"} #ユニクロ
+    user_id               {"2"}
+    buyer_id              {""}
+    size_id               {"5"} #M
+    delivery_payee        {"1"} #送料込み(出品者負担)
+    delivery_time         {"1"} #1~2日で発送
+    delivery_method       {"5"} #クロネコヤマト
   end
 
 end
