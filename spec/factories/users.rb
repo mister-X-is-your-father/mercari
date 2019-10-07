b85242599b26964aed1aec405ee86cb1436188f5
Kento Iwasaki <rockiwasaki1984@gmail.com>
2019-10-07 13:03:42 +0900

    factory_bot user.rbに住所情報追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -13,5 +13,9 @@ FactoryBot.define do
     kana_firstname         {"ハナコ"}
     birth_day              {20000131}
     phone_number           {90912345678}
+    postal_code            {"542-0076"}
+    region                 {"27"}
+    city                   {"大阪市中央区"}
+    block                  {"難波5-1-60"}
   end
 end
